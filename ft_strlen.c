717a4413f77df8cc3c0969ad21d889f1b2c95499
Elif Keten <elifbeyzaketen@gmail.com>
2024-11-06 20:22:26 +0100

    add ft_strlen

---
+++ ft_strlen.c
@@ -0,0 +1,18 @@
+#include "libft.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+/*
+ft_strlen() calculates and returns the length of
+the specified string, excluding the null-terminator.
+*/
